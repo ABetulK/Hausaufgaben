1fcb6cd4b8b083fe58a27369c1a51a3b231d2535
Ayse Betül Keser <121392035+ABetulK@users.noreply.github.com>
2023-02-27 00:06:00 +0100

    YEni satirlar eklendi

--- src/Class1.java
+++ src/Class1.java
@@ -1,5 +1,7 @@
 public class Class1 {
     public static void main(String[] args) {
         System.out.println("Class 1");
+        // GitHup üzerinden yeni bir satır eklendi.
+        System.out.println("Das Leben ist zu kurz, um Java zu lernen.");
     }
 }
